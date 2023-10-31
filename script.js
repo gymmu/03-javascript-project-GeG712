3be763f9bdf729dc41cdf27c552e03f27cc5bae9
GeG712 <e249795@sbl.ch>
2023-10-31 10:09:31 +0100

    E löschen

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if (currentElement === "e") {
+    if (currentElement === "e"|| currentElement === "E") {
       result.push("")
     } else {
       result.push(currentElement)
     }
+
+  }
+
+    
+  return result.join("")
+}
+
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "") {
+      result.push("")
+    } else {
+      result.push(currentElement)
+    }
+
   }
 
     
