f6e365e9b0cc82dceaa0186a76045947d78d6e1a
GeG712 <e249795@sbl.ch>
2023-12-11 17:40:18 +0100

    aufgabe 15

--- script.js
+++ script.js
@@ -276,3 +276,28 @@ export function aufgabe14 (args) {
   return posE 
 }
 
+export function aufgabe15 (args) {
+  const input = args
+  const result = []
+
+  if (input.lastIndexOf(' ') == input.length - 1) {
+      for (let i = 0; i < input.length - 1; i++) {
+        const currentElement = input[i]
+        result.push(currentElement)
+      }
+ }
+   else {
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+  
+      if (currentElement !== " ") {
+        result.push(currentElement)
+      } else {
+        return result.join("")
+      }
+    }
+
+  }
+
+  return result.join("")
+}
