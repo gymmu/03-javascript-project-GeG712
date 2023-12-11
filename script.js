329c2c2fc86329c283567e8bfa63e932ec7b1443
GeG712 <e249795@sbl.ch>
2023-12-11 17:13:18 +0100

    aufgabe 7

--- script.js
+++ script.js
@@ -130,12 +130,20 @@ export function aufgabe06(args) {
 export function aufgabe07 (args) {
   const input = args
   const result = []
-  for (let i = 0; i < input.length; i++) {
-  const currentElement = input[i]
   
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if(currentElement === "u" || input[0] === "U") {
+        if(input[i +1] === "n") {
+        if(input[i + 2] === "d") {
+           return true
+        }
+      }
+    }
   }
-  return result.join("")
+  return false
 }
+
 
 
 
